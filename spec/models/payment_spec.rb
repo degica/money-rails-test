f64507e361f5df38f66e00ca36aac29f707a2262
Chris Salzberg <csalzberg@degica.com>
2017-07-12 16:31:58 +0900

    Test with different default currencies

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,13 +1,29 @@
 require "rails_helper"
 
 describe Payment do
-  it "interprets amount as cents in USD" do
-    payment = Payment.new(amount: 100, currency: "USD")
-    expect(payment.amount.cents).to eq(100)
+  shared_examples_for "cents-based currency converter" do
+    it "interprets amount as cents in USD" do
+      payment = Payment.new(amount: 100, currency: "USD")
+      expect(payment.amount.cents).to eq(100)
+    end
+
+    it "interprets amount as cents in JPY" do
+      payment = Payment.new(amount: 100, currency: "JPY")
+      expect(payment.amount.cents).to eq(100)
+    end
   end
 
-  it "interprets amount as cents in JPY" do
-    payment = Payment.new(amount: 100, currency: "JPY")
-    expect(payment.amount.cents).to eq(100)
+  context "default currency is JPY" do
+    before do
+      Money.default_currency = "JPY"
+    end
+    it_behaves_like "cents-based currency converter"
+  end
+
+  context "default currency is USD" do
+    before do
+      Money.default_currency = "USD"
+    end
+    it_behaves_like "cents-based currency converter"
   end
 end
